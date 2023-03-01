d86cd4ceedcecf597787ee17dfdf9e22e9d60218
IWYNGI <117756770+IWYNGI@users.noreply.github.com>
2023-03-01 14:31:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,13 +18,5 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # fix upx ucl 不存在;
-apt install subversion -y
-svn export --force https://github.com/coolsnowwolf/lede/trunk/tools/upx tools/upx
-svn export --force https://github.com/coolsnowwolf/lede/trunk/tools/ucl tools/ucl
-#n=`sed -n '/^tools-y +=/{p;q}' tools/Makefile`
-#sed -i "/${n}/i\tools-y += ucl upx " tools/Makefile
-#n=`sed -n '/^$(curdir)/{p;q}' tools/Makefile`
-#sed -i '1,/${n}/ {/# builddir dependencies/a\
-#$(curdir)/upx/compile := $(curdir)/ucl/compile
-#}' tools/Makefile
+
 
